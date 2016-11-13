71cb8da1196ddcb647d848d3e36a0b00c31276f5
Fei Qi <fred.qi@ieee.org>
2016-11-13 20:09:50 +0800

    Added an install script for package distribution.

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+__descr__ = "A set of toolkit for faculty members in the Xidian University."
+
+import sys
+
+# Compatibility with Python3
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+
+# Global variable for detecting if we are during setup
+builtins.__XDUFACOOL_SETUP__ = True
+
+DISTNAME = "xdufacool"
+with open("README.md") as readme:
+    LONG_DESCRIPTION = readme.read()
+MAINTAINER = "Fei Qi"
+MAINTAINER_EMAIL = "fred.qi@ieee.org"
+URL= "https://github.com/fredqi/xdufacool"
+with open("LICENSE") as f:
+    LICENSE = f.read()
+
+# Import a restricted version
+import xdufacool
+VERSION = xdufacool.__version__
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+
+def setup_package():
+    """Setup the package."""
+    metadata = dict(name=DISTNAME,
+                    version=VERSION,
+                    maintainer=MAINTAINER,
+                    description=__descr__,
+                    license=LICENSE,
+                    url=URL,
+                    packages=find_packages(include=('xdufacool',)),
+                    # scripts
+                    entry_points={'console_scripts':
+                                      ['xdufacool = xdufacool.homework_manager:check_homeworks']},
+                    # unit tests
+                    test_suite="tests")
+    if (len(sys.argv) >= 2
+        and ('--help' in sys.argv[1:] or sys.argv[1]
+             in ('--help-commands', 'egg_info', '--version'))):
+        metadata['version'] = VERSION
+
+    setup(**metadata)
+
+
+if __name__ == '__main__':
+    setup_package()
---
+++ xdufacool/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.5.0"
+
+import sys
+
+try:
+    # This variable is injected in the __builtins__ by the build
+    # process. It used to enable importing subpackages of sklearn when
+    # the binaries are not built
+    __XDUFACOOL_SETUP__
+except NameError:
+    __XDUFACOOL_SETUP__ = False
+
+if __XDUFACOOL_SETUP__:
+    sys.stderr.write("Partial import of xdufacool during the build process.\n")
+    # We are not importing the rest of the package during the build
+else:
+    __all__ = ["check_homeworks"]
+    from .homework_manager import check_homeworks
