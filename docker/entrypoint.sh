7a96e244a6824148568926d67419349b876c030e
Fred Qi <fred.qi@ieee.org>
2026-03-10 09:58:23 +0800

    refactor: update Dockerfile and entrypoint script to use 'ubuntu' as default APP_USER

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,22 +7,22 @@ XDUFACOOL_BIN=/opt/venv/bin/xdufacool
 
 WORKDIR=${WORKDIR:-/data}
 TASKFILE=${TASKFILE:-PRML.yml}
-APP_USER=${APP_USER:-xdufacool}
+APP_USER=${APP_USER:-ubuntu}
 
 cat > ${CRONTASK} <<EOF
 SHELL=/bin/bash
-PATH=/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+PATH=/usr/local/bin:/usr/sbin:/usr/bin
 CRON_TZ=Asia/Shanghai
 HOME=/home/${APP_USER}
 
 # *  *  *  *  * user-name command to be executed
-${SCHEDULE} ${APP_USER} ${CLI} > /proc/1/fd/1 2>&1
+${SCHEDULE} root su -s /bin/bash -c '${CLI}' ${APP_USER} > /proc/1/fd/1 2>&1
 EOF
 
 cat > ${CLI} <<EOF
 #!/usr/bin/env bash
 set -euo pipefail
-export PATH="/usr/local/bin:/usr/bin:/bin"
+export PATH="/usr/local/bin:/usr/sbin:/usr/bin"
 
 if [ -f "${WORKDIR}/${TASKFILE}" ]; then
     cd "${WORKDIR}"
